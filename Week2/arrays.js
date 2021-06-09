6a9f670e55a23df7c39b3e2db6a4697e4b365a8f
abdi hassan <abdifatahyh@gmail.com>
2021-06-09 19:14:34 +0100

    update

---
+++ Week2/arrays.js
@@ -0,0 +1,28 @@
+// // Example
+// let veg = ["Broccoli", "Onion", "Tomato"];
+// let fruit = ["Apple", "Pear", "Orange"];
+    // console.log(veg[0]);
+    // console.log(fruit[2]);
+
+// for (let hello=0; hello < veg.length; hello++){
+//     console.log(veg[hello])
+// }
+
+
+// // Jordans example
+// let veg = ["Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato"];
+// let fruit = ["Apple", "Pear", "Orange"];
+
+// for (let i = 0; i < veg.length; i++){
+// console.log(veg[i]);
+// console.log(fruit[i]);
+// }
+
+
+// Grid arrays
+let shopping = [
+    ["yogurt", "milk", "apple juice"],
+    ["bananas", "apples", "oranges"],
+    ["rice", "pasta", "noodles"]
+    ]
+    console.log(shopping[1][2]);
